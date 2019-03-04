c84ee58d755d5c49d6bf050d603205bf5aa1c811
Maline30 <malin.subaru@gmail.com>
2019-03-04 11:57:32 +0100

    asd

--- src/main/java/com/kodilla/calculator/Calculate.java
+++ src/main/java/com/kodilla/calculator/Calculate.java
@@ -1,4 +1,16 @@
 package com.kodilla.calculator;
 
 public class Calculate {
+
+    public void someMethod(){
+        System.out.println("It works! ");
+    }
+
+    public static void main(String[] args){
+
+        Calculate calculate = new Calculate();
+        calculate.someMethod();
+    }
+
+
 }
